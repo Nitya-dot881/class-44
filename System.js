4663fc2689c9e6870333321aeb641381106f2bfa
Nitya-dot881 <79143712+Nitya-dot881@users.noreply.github.com>
2021-06-30 06:13:34 -0700

    Add files via upload

---
+++ System.js
@@ -0,0 +1,15 @@
+class System{
+
+    constructor(){}
+
+    // Add code to authenticate the given code and the access codes.
+   authenticate(actualCode,enterCode){
+      if(actualCode===enterCode)
+      return true
+      else
+      return false
+    
+   }
+    
+
+}
